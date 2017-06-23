5cdea631a75ab60e2d02621b69526422414fa0b0
Jess Tucker <jess.tucker@gmail.com>
2017-06-23 16:40:38 -0700

    Adjust new list view to do model validation

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -4,21 +4,24 @@ import random
 
 REPO_URL = 'https://github.com/jesstucker/TDDwPythonV2.git'
 
+
 def deploy():
     site_folder = f'/home/{env.user}/sites/{env.host}'
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
-    _get_lastest_source(source_folder)
+    _get_latest_source(source_folder)
     _update_settings(source_folder, env.host)
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
 
+
 def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
         run(f'mkdir -p {site_folder}/{subfolder}')
 
-def _get_lastest_source(source_folder):
+
+def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
         run(f'cd {source_folder} && git fetch')
     else:
@@ -26,14 +29,15 @@ def _get_lastest_source(source_folder):
     current_commit = local("git log -n 1 --format=%H", capture=True)
     run(f'cd {source_folder} && git reset --hard {current_commit}')
 
+
 def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
-        'ALLOWED_HOSTS = .+$',
+        'ALLOWED_HOSTS =.+$',
         f'ALLOWED_HOSTS = ["{site_name}"]'
     )
-    secret_key_file = source_folder + 'superlists/secret_key.py'
+    secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):  
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
