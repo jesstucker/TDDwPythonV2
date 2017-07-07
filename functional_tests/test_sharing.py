75ef5d95c7c1fa313b4b014802cff3967905800d
Jess Tucker <jess.tucker@gmail.com>
2017-07-06 23:21:49 -0700

    DRYing up sharing feature with Page Pattern TDD

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from .base import FunctionalTest
 from .list_page import ListPage
+from .my_lists_page import MyListsPage
 
 def quit_if_possible(browser):
-	try: browser.quit()
-	except: pass
+	try: 
+		browser.quit()
+	except:
+		pass
 
 class SharingTest(FunctionalTest):
 	def test_can_share_pre_authenticated_session(self):
@@ -21,7 +24,7 @@ class SharingTest(FunctionalTest):
 
 		# Edith goes to the home page and starts a list
 		self.browser = edith_browser
-		# self.browser.get(self.live_server_url) # forgotten in tutorial?
+		self.browser.get(self.live_server_url)
 		list_page = ListPage(self).add_list_item('Get help')
 
 		# She notices a "Share this list" option
@@ -34,3 +37,24 @@ class SharingTest(FunctionalTest):
 		# She shares her list.
 		# The page updates to say that it's shared with Oniciferous:
 		list_page.share_list_with('oniciferous@example.com')
+
+		# Oniciferous now goes to the lists page with his browser
+		self.browser = oni_browser
+		MyListsPage(self).go_to_my_lists_page()
+
+		# He sees Ediths's list in there!
+		self.browser.find_element_by_link_text('Get help').click()
+
+		# On the list page, Oniciferous can see that it says that it's Edith's list
+		self.wait_for(lambda: self.assertEqual(
+			list_page.get_list_owner(),
+			'edith@example.com'
+		))
+
+		# He adds an item item to the list
+		list_page.add_list_item('Hi Edith!')
+
+		# When Edith refreshes the page, she sees Oniciferous's addition
+		self.browser = edith_browser
+		self.browser.refresh()
+		list_page.wait_for_row_in_list_table('Hi Edith!', 2)
